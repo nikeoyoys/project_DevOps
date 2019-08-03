42d1851b95e6a6b57c023f1709f6340cc2796bfc
Your Name <you@example.com>
2019-08-03 02:32:12 -0700

    update app.py Dockerfile VERSION content

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.7
 ADD . /code
 WORKDIR /code
 RUN pip install flask 
+RUN pip install redis
 CMD python app.py
--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from redis import Redis
+
 app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello():
+    redis.incr('hits')
+    return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0',port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
